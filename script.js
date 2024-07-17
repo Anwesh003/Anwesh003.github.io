4df981880f9e1e8d97b886e481a76d8ad47f6230
Anwesh Krishna B <anweshkrishnab6324@gmail.com>
2024-07-17 14:59:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to toggle project details
+function toggleDetails(projectId) {
+    const details = document.getElementById(projectId + '-details');
+    details.classList.toggle('active');
+}
